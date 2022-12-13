e0e1934f7c44bb6d21adbed0e64c2a0c8589e748
Weronika Julia Jastrzębska <werainteria@gmail.com>
2022-12-13 22:14:42 +0100

    element diagonalny

---
+++ lmp9/src/eliminacja.c
@@ -0,0 +1,36 @@
+#include "eliminacja.h"
+
+
+int eliminacja (Matrix *mac, Matrix *b, Matrix *x){
+	int n=mac->r;
+	int k;
+	int w;
+	int i;
+	double m;
+	double *temp;
+	double *tymb;
+	for (k=0; k<n-1; k++){
+		int kmax;
+		for (w=k+1; w<n; w++){
+			if (fabs(mac->data[w][k])>fabs(mac->data[kmax][k]) )
+				kmax=w;
+			if(kmax!=k){
+				temp=mac->data[k];
+				mac->data[k]=mac->data[kmax];
+				mac->data[kmax]=temp;
+				tymb=b->data[k];
+				b->data[k]=b->data[kmax];
+				b->data[kmax]=tymb;		
+			}
+		}
+		for (w=k+1; w<n; w++){
+			if(mac->data[k][k]==0)
+				return 1;
+			m=mac->data[w][k]/mac->data[k][k];
+			for (i=k; i<n; i++)
+				mac->data[w][i]-=m*mac->data[k][i];
+			b->data[w][0]-=m*b->data[k][0];
+			}
+		}
+		return 0;
+	}
---
+++ lmp9/src/eliminacja.h
@@ -0,0 +1,10 @@
+#include "mat_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// return 1 -> nie da się uniknąć dzielenia przez 0
+// return 0 -> działa poprawnie
+
+
+int eliminacja (Matrix *mac, Matrix *b, Matrix *x);
