c2a3d6709baeeae4cd07c313f031507d6c483d8b
Weronika Julia Jastrzębska <werainteria@gmail.com>
2022-12-13 20:40:55 +0100

    implementacja eliminacji gaussa

--- lmp9/src/gauss.c
+++ lmp9/src/gauss.c
@@ -5,10 +5,21 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
+    		int row = mat->r;
+		int i;
+		int j;
+		int z;
+		double m;
+		for (i=0; i<row-1; i++){
+			for(j=i+1; j<row; j++){
+				if(mat->data[i][i]==0)
+					return 1;
+				m=mat->data[j][i]/mat->data[i][i];
+				for(z=i; z<row; z++)
+					mat->data[j][z]-=m*mat->data[i][z];
+				b->data[j][0]-=m * b->data[i][0];
+			}
+		}
 		return 0;
 }
 
